fbc33487b8903d74ba8d06f40ad40e42e7d2b652
Mark Langer <mark-langer@users.noreply.github.com>
2022-01-03 14:14:35 +0100

    fix(clipboard): Remove focus() call when pasting.
    When pasting, this caused the quill editor to be blurred and then re-focused.

--- modules/clipboard.js
+++ modules/clipboard.js
@@ -110,7 +110,7 @@ class Clipboard extends Module {
     let range = this.quill.getSelection();
     let delta = new Delta().retain(range.index);
     let scrollTop = this.quill.scrollingContainer.scrollTop;
-    this.container.focus();
+    // this.container.focus();
     this.quill.selection.update(Quill.sources.SILENT);
     setTimeout(() => {
       delta = delta.concat(this.convert()).delete(range.length);
@@ -118,7 +118,7 @@ class Clipboard extends Module {
       // range.length contributes to delta.length()
       this.quill.setSelection(delta.length() - range.length, Quill.sources.SILENT);
       this.quill.scrollingContainer.scrollTop = scrollTop;
-      this.quill.focus();
+      // this.quill.focus();
     }, 1);
   }
 
